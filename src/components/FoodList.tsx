8073120616063e81afac067b27494e0ab6b84409
Yizhen Zheng <131596227+Yizhen-Zheng@users.noreply.github.com>
2025-02-05 13:51:46 +0900

    Deploy with feature (#21)
    * add fetch helpers folder and remove unused value/func
    
    * change import css file
    
    * change import css file

--- src/components/FoodList.tsx
+++ src/components/FoodList.tsx
@@ -1,7 +1,16 @@
 type FoodListProps = {};
-
-const FoodList: React.FC<FoodListProps> = (props) => {
+//  (props) temp removed for build
+const FoodList: React.FC<FoodListProps> = () => {
   return (
+
+<!--     <div className="food-list">
+      <div className="food">ramen</div>
+      <div className="food">yakiniku</div>
+      <div className="food">mexican</div>
+      <div className="food">italian</div>
+      <div className="food">pizza</div>
+    </div> -->
+
     <ul className='food-list l-food-list'>
       <li className='food'>ramen</li>
       <li className='food'>yakiniku</li>
@@ -9,6 +18,7 @@ const FoodList: React.FC<FoodListProps> = (props) => {
       <li className='food'>italian</li>
       <li className='food'>pizza</li>
     </ul>
+
   );
 };
 
