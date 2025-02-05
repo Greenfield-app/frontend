26979d46dd8cbf67a3bc14927db5d59de6f9b55f
Yizhen Zheng <131596227+Yizhen-Zheng@users.noreply.github.com>
2025-02-05 14:09:41 +0900

    Remove incorrect comment out (#22)
    * add fetch helpers folder and remove unused value/func
    
    * change import css file
    
    * change import css file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -26,11 +26,7 @@ function App() {
   // return
   return (
     <>
-
-<!--       <nav className="l-header"> -->
-
-      <nav className='l-header header'>
-
+      <nav className="l-header header">
         <h1>What's Eat</h1>
       </nav>
       <p>{fetchedResult}</p>
--- src/components/FoodList.tsx
+++ src/components/FoodList.tsx
@@ -2,23 +2,13 @@ type FoodListProps = {};
 //  (props) temp removed for build
 const FoodList: React.FC<FoodListProps> = () => {
   return (
-
-<!--     <div className="food-list">
-      <div className="food">ramen</div>
-      <div className="food">yakiniku</div>
-      <div className="food">mexican</div>
-      <div className="food">italian</div>
-      <div className="food">pizza</div>
-    </div> -->
-
-    <ul className='food-list l-food-list'>
-      <li className='food'>ramen</li>
-      <li className='food'>yakiniku</li>
-      <li className='food'>mexican</li>
-      <li className='food'>italian</li>
-      <li className='food'>pizza</li>
+    <ul className="food-list l-food-list">
+      <li className="food">ramen</li>
+      <li className="food">yakiniku</li>
+      <li className="food">mexican</li>
+      <li className="food">italian</li>
+      <li className="food">pizza</li>
     </ul>
-
   );
 };
 
