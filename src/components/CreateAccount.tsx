6ed415388faf14768969e2d6c935359862146d2f
Matt K <matthew.b.kurtti@gmail.com>
2025-02-06 15:52:26 +0900

    feat: add title to loginpage and signup page

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -3,16 +3,22 @@ import "../styles/modules/createaccount.css";
 //  (props) temp removed for build
 const CreateAccount: React.FC<CreateAccountProps> = (props) => {
   return (
-    <div className="createaccount">
-      <h1>Create Account</h1>
-      <label htmlFor="">Username: </label>
-      <input type="text" />
-      <label htmlFor="">Password: </label>
-      <input type="text" />
-      <label htmlFor="">Confirm Password: </label>
-      <input type="text" />
-      <button>Submit</button>
-    </div>
+    <>
+      <nav className="l-header header">
+        <h1>What's Eat</h1>
+      </nav>
+
+      <div className="createaccount">
+        <h1>Create Account</h1>
+        <label htmlFor="">Username: </label>
+        <input type="text" />
+        <label htmlFor="">Password: </label>
+        <input type="text" />
+        <label htmlFor="">Confirm Password: </label>
+        <input type="text" />
+        <button>Submit</button>
+      </div>
+    </>
   );
 };
 
