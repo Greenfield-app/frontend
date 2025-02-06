678a60c90f45ec5df72348f59f81f4849ffc9ef7
Garett Takeyasu <takeyasu.garett@gmail.com>
2025-02-06 12:15:22 +0900

    feat: add eat button on the foodCards to indicated it was eaten today

--- src/components/FoodList.tsx
+++ src/components/FoodList.tsx
@@ -1,5 +1,6 @@
 import { FoodInfo } from "../vite-env";
 import trashIcon from "../assets/icons/icon-monster-trash.svg";
+import eatIcon from "../assets/icons/eat.svg";
 import { useEffect } from "react";
 
 // types from App.tsx
@@ -28,20 +29,32 @@ const FoodList: React.FC<FoodListProps> = (props) => {
     props.setFoods(sampleFoods);
   }, []);
 
-  const handleDeleteFood = (foodCardId: number) => {
-    console.log("food card with id ", foodCardId, " was deleted!");
+  const handleDeleteFood = (foodCard: FoodInfo) => {
+    console.log(foodCard, " was deleted!");
+    // delete in database
+  };
+
+  const handleEatFood = (foodCard: FoodInfo) => {
+    console.log(foodCard, " was eaten!");
+    // send to database Records of when eaten
   };
 
   return (
-    <ul className="food-list l-food-list">
+    <ul className='food-list l-food-list'>
       {props.foods.map((food) => (
-        <li key={food.id} className="food">
-          <h3 className="food-title">{food.name}</h3>
+        <li key={food.id} className='food'>
           <img
-            className="food-delete-icon"
+            className='food-eat-icon'
+            src={eatIcon}
+            alt='eat icon'
+            onClick={() => handleEatFood(food)}
+          />
+          <h3 className='food-title'>{food.name}</h3>
+          <img
+            className='food-delete-icon'
             src={trashIcon}
-            alt="red trash icon"
-            onClick={() => handleDeleteFood(food.id)}
+            alt='red trash icon'
+            onClick={() => handleDeleteFood(food)}
           />
         </li>
       ))}
