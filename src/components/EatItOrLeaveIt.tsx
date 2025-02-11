b036144a77dc19e0b9a5ff4191297573786bb4ad
Yizhen Zheng <131596227+Yizhen-Zheng@users.noreply.github.com>
2025-02-11 15:10:10 +0900

    Merge with style ,fix: next food btn (#105)
    * feat: basic setup prepare for fetch food info and data for single choice
    
    * feat: be able to send record when click eat btn
    
    * fix: display current user after new registration
    
    * feat: be able to fetch data of random food
    
    * feat: display random food in eat it or leave it
    
    * feat: fetch random food
    
    * get id location
    
    * feat: be able to fetch food and nearby restaurant
    
    * display address of restaurants
    
    * display restaurants with overflow auto
    
    * remove unused value
    
    * catch cors block and access limit error by returning default location(tokyo)
    
    * save

--- src/components/EatItOrLeaveIt.tsx
+++ src/components/EatItOrLeaveIt.tsx
@@ -1,7 +1,7 @@
 import whatsEat from "../assets/icons/whatsEat-icon.png";
 import eatIt from "../assets/icons/eat-it.png";
 import leaveIT from "../assets/icons/leave-it.png";
-import { useState, useEffect, MouseEvent } from "react";
+import { useState, useEffect } from "react";
 import {
   UserInfo,
   FoodInfoDisplay,
@@ -74,56 +74,56 @@ const EatItOrLeaveIt: React.FC<EatItOrLeaveItProps> = ({
         }
       }
       //if guest or user not exist, just get another random food
-      getNextFood();
     }
+    getNextFood();
   };
 
   const address =
     "Ramen Issei, 3 Chome-40-23 Maeharacho, Koganei, Tokyo 184-0013";
   return (
     <>
-      <nav className='l-header header'>
-        <header className='home-header'>
-          <img className='whatseat-icon' src={whatsEat} />
+      <nav className="l-header header">
+        <header className="home-header">
+          <img className="whatseat-icon" src={whatsEat} />
           <h1 onClick={() => setView("home")}>WhatsEat</h1>
         </header>
-        <div className='username-and-logout'>
+        <div className="username-and-logout">
           <h1>{currentUser.userName}</h1>
-          <h3 className='nav-text' onClick={() => setView("loginpage")}>
+          <h3 className="nav-text" onClick={() => setView("loginpage")}>
             Logout
           </h3>
         </div>
       </nav>
-      <div className='eatitleaveit-content-container'>
-        <h1 className='eatitorleaveit-title'>Eat it or leave it</h1>
+      <div className="eatitleaveit-content-container">
+        <h1 className="eatitorleaveit-title">Eat it or leave it</h1>
         {/* <h2>{randomFood.name}</h2> currently doesn't work, will need to refactor*/}
 
-        <div className='eatitorleaveit-container'>
+        <div className="eatitorleaveit-container">
           {randomFood && (
             <div>
               <img
-                className='eat-leave-img'
+                className="eat-leave-img"
                 src={randomFood.image}
                 alt={randomFood.foodName || "Food Name"}
               />
-              <h3 className='food-title'>{randomFood.foodName}</h3>
-              <div className='eat-or-leave-btns'>
+              <h3 className="food-title">{randomFood.foodName}</h3>
+              <div className="eat-or-leave-btns">
                 <img
-                  className='eat-it-icon'
+                  className="eat-it-icon"
                   src={eatIt}
-                  alt='eat it icon'
-                  onClick={(e) => handleEatFood(e)}
+                  alt="eat it icon"
+                  onClick={() => handleEatFood()}
                 />
                 <p>Or</p>
                 <img
                   src={leaveIT}
-                  alt='leave it icon'
-                  className='leave-it-icon'
-                  onClick={(e) => handleDeleteFood(e)}
+                  alt="leave it icon"
+                  className="leave-it-icon"
+                  onClick={() => handleDeleteFood()}
                 />
               </div>
               {restaurantsInfo !== null && (
-                <ul className='single-restaurant'>
+                <ul className="single-restaurant">
                   {restaurantsInfo.map((restaurant) => {
                     return (
                       <li key={restaurant.name}>
@@ -131,7 +131,7 @@ const EatItOrLeaveIt: React.FC<EatItOrLeaveItProps> = ({
                           href={`https://www.google.com/maps/search/?q=${
                             restaurant.name + ", " + restaurant.address
                           }`}
-                          target='blank'
+                          target="blank"
                         >
                           {restaurant.name}
                         </a>
